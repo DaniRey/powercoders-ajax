ac1d454f28d5d3dd7b56bbf7c66fe6d17c13e97d
Dani Rey <dani.rey.ch@gmail.com>
2017-02-14 00:30:20 +0100

    simple weather page

---
+++ weather/application.js
@@ -0,0 +1,28 @@
+function Weather(weather) {
+  this.current = function(){
+    var curr = weather.data.currentHour
+    return "<p>The current weather condition is " + curr.condition +" with a temperature of " + curr.temperatureInCelsius.value + "</p>"
+  }
+  this.forecasts = function(){
+    var text = "";
+    for(forecast of weather.data.forecasts) {
+      text += "<p>on " + forecast.date + " the weather condition is " + forecast.condition + "</p>";
+    }
+    return text;
+  }
+}
+
+$(document).ready(function() {
+  $.ajax(
+    'http://2016.jungfrau.ch/api/weather/v01/average/jungfraujoch',
+    {
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function(json){
+        var weather = new Weather(json);
+        $('#current_weather').html(weather.current);
+        $('#forecasts').html(weather.forecasts);
+      }
+    }
+  )
+});
